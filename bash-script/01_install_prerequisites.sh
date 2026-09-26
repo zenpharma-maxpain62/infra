1c7b10cf25dc87931092681ea9ecd02b79a8ac73
maxpain62 <maxpain62@gmail.com>
2026-09-26 11:03:29 +0530

    feat: added functions in 01_install_prerequisites.sh bash script

--- bash-script/01_install_prerequisites.sh
+++ bash-script/01_install_prerequisites.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+addingHelmRepo()
+{
 echo "------------------------------"
 echo "Adding helm repositories"
 echo "------------------------------"
@@ -9,74 +12,81 @@ helm repo update
 echo "------------------------------------------"
 echo "helm repositories added successfully"
 echo "------------------------------------------"
+}
 
 
-helm list -A | grep -i aws-load-balancer-controller > /dev/null 2>&1
-export AWS_LB_CONTROLLER_STATUS=$(echo $?)
-kubectl get pods -A | grep -i "aws-load-balancer-controller-" > /dev/null 2>&1 > /dev/null 2>&1
-export AWS_LB_POD_STATUS=$(echo $?)
+installAWSLoadBalancer()
+{
+    helm list -A | grep -i aws-load-balancer-controller > /dev/null 2>&1
+    export AWS_LB_CONTROLLER_STATUS=$(echo $?)
+    kubectl get pods -A | grep -i "aws-load-balancer-controller-" > /dev/null 2>&1 > /dev/null 2>&1
+    export AWS_LB_POD_STATUS=$(echo $?)
+    if [[ $AWS_LB_CONTROLLER_STATUS -eq 0 ]] && [[ $AWS_LB_POD_STATUS -eq 0 ]]; then
+    	echo "AWS Load Balancer Controller is installed"
+    else
+    	echo "------------------------------------------"
+    	echo "Installing AWS Load Balancer Controller"
+    	echo "------------------------------------------"
+    	VPCID=$(aws eks describe-cluster --name zenpharma-dev-cluster --region ap-south-1 --query "cluster.resourcesVpcConfig.vpcId" --output text)
+    	ALB_Controller_Role_ARN=$(aws iam list-roles --query "Roles[?contains(RoleName, 'alb-controller')].Arn" --output text)
+    	helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-controller \
+    	--namespace kube-system \
+    	--set clusterName=zenpharma-dev-cluster  \
+    	--set region=ap-south-1 \
+    	--set vpcId=$VPCID \
+    	--set serviceAccount.create=true \
+      	--set serviceAccount.name=aws-load-balancer-controller \
+      	--set "serviceAccount.annotations.eks\.amazonaws\.com/role-arn=$ALB_Controller_Role_ARN" \
+      	--wait --timeout 5m 
+    	if [[ $? == 0 ]]; then
+    	echo "------------------------------------------"
+    	echo "AWS Load Balancer Controller installed successfully"
+    	echo "------------------------------------------"
+    	else 
+            echo -e "\e[31m------------------------------------------------------\e[0m"
+            echo -e "\e[31mPlease verify if AWS Load Balancer Controller Installed successfully or not\e[0m"
+            echo -e "\e[31m------------------------------------------------------\e[0m"
+    	fi
+    fi
+    unset $AWS_LB_CONTROLLER_STATUS
+    unset $AWS_LB_POD_STATUS
+}
 
-if [[ $AWS_LB_CONTROLLER_STATUS -eq 0 ]] && [[ $AWS_LB_POD_STATUS -eq 0 ]]; then
-	echo "AWS Load Balancer Controller is installed"
-else
-	echo "------------------------------------------"
-	echo "Installing AWS Load Balancer Controller"
-	echo "------------------------------------------"
-	VPCID=$(aws eks describe-cluster --name zenpharma-dev-cluster --region ap-south-1 --query "cluster.resourcesVpcConfig.vpcId" --output text)
-	ALB_Controller_Role_ARN=$(aws iam list-roles --query "Roles[?contains(RoleName, 'alb-controller')].Arn" --output text)
-	helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-controller \
-	--namespace kube-system \
-	--set clusterName=zenpharma-dev-cluster  \
-	--set region=ap-south-1 \
-	--set vpcId=$VPCID \
-	--set serviceAccount.create=true \
-  	--set serviceAccount.name=aws-load-balancer-controller \
-  	--set "serviceAccount.annotations.eks\.amazonaws\.com/role-arn=$ALB_Controller_Role_ARN" \
-  	--wait --timeout 5m 
-	if [[ $? == 0 ]]; then
-	echo "------------------------------------------"
-	echo "AWS Load Balancer Controller installed successfully"
-	echo "------------------------------------------"
-	else 
-        echo -e "\e[31m------------------------------------------------------\e[0m"
-        echo -e "\e[31mPlease verify if AWS Load Balancer Controller Installed successfully or not\e[0m"
-        echo -e "\e[31m------------------------------------------------------\e[0m"
-	fi
-fi
-unset $AWS_LB_CONTROLLER_STATUS
-unset $AWS_LB_POD_STATUS
+
+installArgoCD()
+{
+    helm list -A | grep -i argocd > /dev/null 2>&1
+    export ARGO_HELM_STATUS=$(echo $?)
+    kubectl get pods -A | grep -i "argocd-" > /dev/null 2>&1 > /dev/null 2>&1
+    export ARGO_POD_STATUS=$(echo $?)
+    if [[ $ARGO_HELM_STATUS -eq 0 ]] && [[ $ARGO_POD_STATUS -eq 0 ]]; then
+    	echo "argocd is installed"
+    else
+    	echo "------------------------------------------"
+    	echo "Installing argocd"
+    	echo "------------------------------------------"
+    	helm upgrade --install argocd argo/argo-cd --namespace argocd --create-namespace --wait --timeout 10m
+    	if [[ $? == 0 ]]; then
+    	echo "------------------------------------------"
+    	echo "argocd installed successfully"
+    	echo "------------------------------------------"
+    	else 
+            echo -e "\e[31m------------------------------------------------------\e[0m"
+            echo -e "\e[31mPlease verify if AWS Load Balancer Controller Installed successfully or not\e[0m"
+            echo -e "\e[31m------------------------------------------------------\e[0m"
+    	fi
+    fi
+    unset $ARGO_HELM_STATUS
+    unset $ARGO_POD_STATUS
+}
 
 
-helm list -A | grep -i argocd > /dev/null 2>&1
-export ARGO_HELM_STATUS=$(echo $?)
-kubectl get pods -A | grep -i "argocd-" > /dev/null 2>&1 > /dev/null 2>&1
-export ARGO_POD_STATUS=$(echo $?)
-
-if [[ $ARGO_HELM_STATUS -eq 0 ]] && [[ $ARGO_POD_STATUS -eq 0 ]]; then
-	echo "argocd is installed"
-else
-	echo "------------------------------------------"
-	echo "Installing argocd"
-	echo "------------------------------------------"
-	helm upgrade --install argocd argo/argo-cd --namespace argocd --create-namespace --wait --timeout 10m
-	if [[ $? == 0 ]]; then
-	echo "------------------------------------------"
-	echo "argocd installed successfully"
-	echo "------------------------------------------"
-	else 
-        echo -e "\e[31m------------------------------------------------------\e[0m"
-        echo -e "\e[31mPlease verify if AWS Load Balancer Controller Installed successfully or not\e[0m"
-        echo -e "\e[31m------------------------------------------------------\e[0m"
-	fi
-fi
-unset $ARGO_HELM_STATUS
-unset $ARGO_POD_STATUS
-
+installExternalSecretOperator()
+{
 helm list -A | grep -i external-secrets > /dev/null 2>&1
 export EXTERNAL_SECRET_OPERATOR_STATUS=$(echo $?)
 kubectl get pods -A | grep -i "external-secrets-" > /dev/null 2>&1 > /dev/null 2>&1
 export EXTERNAL_SECRET_POD_STATUS=$(echo $?)
-
 if [[ $EXTERNAL_SECRET_OPERATOR_STATUS -eq 0 ]] && [[ $EXTERNAL_SECRET_POD_STATUS -eq 0 ]]; then
 	echo "External Secrets Operator is installed"
 else
@@ -100,11 +110,22 @@ else
 fi
 unset $EXTERNAL_SECRET_OPERATOR_STATUS
 unset $EXTERNAL_SECRET_POD_STATUS
+}
 
 
+verification()
+{
 echo "------------------------------------------"
 echo "Verification"
 echo "------------------------------------------"
 kubectl get pods -n kube-system -l app.kubernetes.io/name=aws-load-balancer-controller
 kubectl get pods -n argocd
 kubectl get pods -n external-secrets
+}
+
+#calling functions
+addingHelmRepo
+installAWSLoadBalancer
+installArgoCD
+installExternalSecretOperator
+verification
