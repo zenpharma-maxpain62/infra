1db96ee25ac5a58a8acaca69dd105f8851e1f453
maxpain62 <maxpain62@gmail.com>
2026-09-26 12:51:06 +0530

    feat: register gitops repo with argocd

--- bash-script/01_install_prerequisites.sh
+++ bash-script/01_install_prerequisites.sh
@@ -123,9 +123,42 @@ kubectl get pods -n argocd
 kubectl get pods -n external-secrets
 }
 
+registerGitopsRepository()
+{
+	echo "------------------------------------------"
+	echo "Register the GitOps Repository in ArgoCD"
+	echo "------------------------------------------"
+	read -rsp "Enter GitHub PAT: " GITHUB_PAT
+	# Create the secret with repo credentials
+	kubectl create secret generic zen-gitops-repo \
+	  --namespace argocd \
+	  --from-literal=type=git \
+	  --from-literal=url=https://github.com/zenpharma-maxpain62/gitops.git \
+	  --from-literal=username=maxpain62 \
+	  --from-literal=password=$GITHUB_PAT \
+	  --dry-run=client -o yaml | kubectl apply -f -
+
+	# Label it so ArgoCD recognizes it as a repository
+	kubectl label secret zen-gitops-repo \
+	  argocd.argoproj.io/secret-type=repository \
+	  --namespace argocd \
+	  --overwrite
+	kubectl get secret zen-gitops-repo -n argocd > /dev/null 2>&1
+	if [[ $? == 0 ]]; then
+	echo "------------------------------------------"
+	echo "GitOps Repository in ArgoCD register successfully"
+	echo "------------------------------------------"
+	else
+	echo -e "\e[31m------------------------------------------------------\e[0m"
+    echo -e "\e[31mFailed to register GitOps repository in ArgoCD\e[0m"
+    echo -e "\e[31m------------------------------------------------------\e[0m"
+	fi
+}
+
 #calling functions
 addingHelmRepo
 installAWSLoadBalancer
 installArgoCD
 installExternalSecretOperator
 verification
+registerGitopsRepository
